3e55f39eb548aa69780fff1cbfb10143a2226147
2007param <2007parampatel@gmail.com>
2024-12-21 21:03:44 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,24 +1,14 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🧬Bioinformatics Tool")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
-st.set_page_config(
-    page_title="Bioinformatics Tool",
-    page_icon="🧬",
-    layout="wide",
-)
-
-# Title and Introduction
-st.title("Bioinformatics Tool 🧬")
 st.write("""
 Welcome to the Bioinformatics Tool!  
 Analyze DNA sequences using this interactive platform. You can perform nucleotide counting, k-mer analysis, 
 and search for motifs or genes.  
 """)
-
-# Sidebar for input
 st.sidebar.header("Input Section")
 sequence = st.sidebar.text_area(
     "Enter a DNA sequence (A, T, G, C only):",
