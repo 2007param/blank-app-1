a63e3047e3866c197ffc5a410c2c74de5e42eaf7
2007param <2007parampatel@gmail.com>
2024-12-24 15:24:29 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,16 +4,16 @@ from collections import Counter
 from PIL import Image
 
 image = Image.open(r"C:\Users\2007p\Downloads\download (28).jpeg")
-image1 = Image.open("C:\Users\2007p\Downloads\download (29).jpeg")
-image2 = Image.open("C:\Users\2007p\Downloads\download (30).jpeg")
-image3 = Image.open("C:\Users\2007p\Downloads\download (31).jpeg")
-image4 = Image.open("C:\Users\2007p\Downloads\download (4).jpeg")
-image5 = Image.open("C:\Users\2007p\Downloads\download (5).jpeg")
-image6 = Image.open("C:\Users\2007p\Downloads\download (32).jpeg")
-image7 = Image.open("C:\Users\2007p\Downloads\download (33).jpeg")
-image8 = Image.open("C:\Users\2007p\Downloads\download (6).jpeg")
-image9 = Image.open("C:\Users\2007p\Downloads\download (34).jpeg")
-image10 = Image.open("C:\Users\2007p\Downloads\download (35).jpeg")
+image1 = Image.open(r"C:\Users\2007p\Downloads\download (29).jpeg")
+image2 = Image.open(r"C:\Users\2007p\Downloads\download (30).jpeg")
+image3 = Image.open(r"C:\Users\2007p\Downloads\download (31).jpeg")
+image4 = Image.open(r"C:\Users\2007p\Downloads\download (4).jpeg")
+image5 = Image.open(r"C:\Users\2007p\Downloads\download (5).jpeg")
+image6 = Image.open(r"C:\Users\2007p\Downloads\download (32).jpeg")
+image7 = Image.open(r"C:\Users\2007p\Downloads\download (33).jpeg")
+image8 = Image.open(r"C:\Users\2007p\Downloads\download (6).jpeg")
+image9 = Image.open(r"C:\Users\2007p\Downloads\download (34).jpeg")
+image10 = Image.open(r"C:\Users\2007p\Downloads\download (35).jpeg")
 
 def display_image():
     st.image(image, caption="Title Page", use_column_width=True)
