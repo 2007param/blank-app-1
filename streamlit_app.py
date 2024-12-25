c518b82d40a53622106d2dd4f74ab5261c57bc75
2007param <2007parampatel@gmail.com>
2024-12-24 16:31:32 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -183,6 +183,7 @@ def title_page():
     apply_background_color("#D6EAF8")  
     st.markdown("<h1 style='text-align: center;'>🧬 Bioinformatics Tool</h1>", unsafe_allow_html=True)
     st.markdown("<p style='text-align: center;'>Welcome to the Bioinformatics Tool. This app provides several bioinformatics analysis tools such as Nucleotide count, K-mer analysis, Gene Finding, and more. Select an option from the menu to get started!</p>", unsafe_allow_html=True)
+    st.markdown("<h1 style='text-align: center;'>ONLY USE CAPITAL LETTERS WHEN TYPING NUCLEOTIDE BASES</h1>", unsafe_allow_html=True)
     display_image()
     footer()
 def about_us_page():
