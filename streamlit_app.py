8ce1ffe08b89a6e603d6e60896f9632545a379f5
2007param <2007parampatel@gmail.com>
2025-01-02 16:41:51 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -107,7 +107,7 @@ def homepage():
     )
 
 # Create a button in one of the columns
-    col1, col2, col3 = st.columns([2,2,1])
+    col1, col2, col3 = st.columns([2,2,2])
     with col2:
         if st.button("Get Started"):
             st.success("Thank you for clicking! Let’s get started. First, click the arrow on the top left! Then, open the sidebar! Lastly, use your favorite bioinformatics tools!")
