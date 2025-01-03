1204e20c311671ca4af45f4a5268b9836a9172f4
2007param <2007parampatel@gmail.com>
2025-01-02 16:39:48 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -105,11 +105,11 @@ def homepage():
         """,
         unsafe_allow_html=True
     )
-    col1, col2, col3, col4, col5 = st.columns([3, 2, 2, 2, 3])
+    col1, col2, col3, col4, col5 = st.columns([1,2,1])
 
 # Create a button in one of the columns
     col1, col2, col3, col4, col5 = st.columns([1,2,3,4,5])
-    with col3:
+    with col2:
         if st.button("Get Started"):
             st.success("Thank you for clicking! Let’s get started. First, click the arrow on the top left! Then, open the sidebar! Lastly, use your favorite bioinformatics tools!")
     
